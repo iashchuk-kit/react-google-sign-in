d6b6ea0262b97ce4e7a7da8c5b06bcc69cd12c29
iashchuk <221box@list.ru>
2019-02-16 05:40:51 +0300

    Добавляет компонент App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import Title from "./Title";
+import Button from "./Button";
+
+import styles from "../styles/App.module.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">Google Sign</header>
+      <div className={styles.app}>
+        <header className={styles.header}>
+          <Title />
+          <Button text="Log in" />
+        </header>
       </div>
     );
   }
